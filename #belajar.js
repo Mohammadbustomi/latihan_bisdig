020b1cd299d27ce38bd2fd7867262f4bdec5f8ac
Mohammadbustomi <bustomi4054@gmail.com>
2025-09-10 15:14:53 +0700

    belajar_js

--- #belajar.js
+++ #belajar.js
@@ -1,1 +1,9 @@
-alert("Hi there.");
+window.onload = function(){
+    alert('selamat datang di websitetomi')
+};
+confirm("apakah km tau hal apa yang menyenangkan");
+Date(10)
+Math.random()
+Math.round(4.6)
+parseInt("234")
+setTimeout(function(){alert("selamat datang di website inii");},3000);
